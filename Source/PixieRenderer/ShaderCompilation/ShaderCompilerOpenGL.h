64c4d343b94a16982899ec3c599af0f4644cdcb6
asuart <asuart@yandex.ru>
2026-09-18 19:49:57 +0300

    Fix gcc build

--- Source/PixieRenderer/ShaderCompilation/ShaderCompilerOpenGL.h
+++ Source/PixieRenderer/ShaderCompilation/ShaderCompilerOpenGL.h
@@ -1,8 +1,18 @@
 #pragma once
 #include <glad/glad.h>
 
+<<<<<<< Updated upstream:Source/PixieRenderer/ShaderCompilation/ShaderCompilerOpenGL.h
 #include "Pixierenderer/Renderer/OpenGL/OpenGLGraphicsProgram.h"
 #include "Pixierenderer/Renderer/OpenGL/OpenGLComputeProgram.h"
+=======
+<<<<<<< Updated upstream:Source/PixieRenderer/Renderer/OpenGL/ShaderCompilationOpenGL.h
+#include "OpenGLGraphicsProgram.h"
+#include "OpenGLComputeProgram.h"
+=======
+#include "PixieRenderer/Renderer/OpenGL/OpenGLGraphicsProgram.h"
+#include "PixieRenderer/Renderer/OpenGL/OpenGLComputeProgram.h"
+>>>>>>> Stashed changes:Source/PixieRenderer/ShaderCompilation/ShaderCompilerOpenGL.h
+>>>>>>> Stashed changes:Source/PixieRenderer/Renderer/OpenGL/ShaderCompilationOpenGL.h
 
 namespace PixieRenderer {
 
